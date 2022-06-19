d021d983263a5e89291e61363dcb3184e76f5064
Cristiano M <moreiracristiano702@gmail.com>
2022-06-19 00:48:47 -0300

    .

---
+++ pyshark/src/main.py
@@ -0,0 +1,30 @@
+import pyshark
+import json
+
+# Reading a captured file
+capturedFile = pyshark.FileCapture("../pcap_files/captura1.pcap")
+
+# Load the conference file
+with open("../data/conferenceData.json") as conference_file:
+    data = json.load(conference_file)
+
+# Simple print json
+print(data)
+
+# Print formatted JSON
+print(json.dumps(data, indent=2))
+
+# Iteration in all packets of pcap file
+for packet in capturedFile:
+    try:
+        print(packet)
+        protocol = packet.transport_layer
+        src_addr = packet.ip.src
+        src_port = packet[protocol].srcport
+        dst_addr = packet.ip.dst
+        dst_port = packet[protocol].dstport
+        print(f"Src IP: {src_addr}:{src_port}\nDst IP: {dst_addr}:{dst_port}\nProtocol: {protocol} \n")
+    except AttributeError:
+        print("Something went wrong")
+
+
---
+++ pyshark/src/readme.txt
@@ -0,0 +1,3 @@
+								Anotacoes
+
+Para o python entender o json as virgulas devem ser minimas. Nas linhas 5, 6, 7, por exemplo, nao podemos colocar virgula pois o interpretador buga
